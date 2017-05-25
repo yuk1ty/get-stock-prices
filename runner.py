936da1451c5a4522d6716eec91af64082439b1c9
Yuki <yuki.mul.tiplus@gmail.com>
2017-05-26 01:06:58 +0900

    動いていなかったので修正する

---
+++ runner.py
@@ -0,0 +1,3 @@
+import stock_price_scraping as sps
+
+sps.getStockData("AAPL", '2016-01-01', '2017-01-01')
--- stock_price_scraping.py
+++ stock_price_scraping.py
@@ -2,5 +2,5 @@ import pandas_datareader as data
 
 DATA_SOURCE = 'google'
 
-def getStockDate(ticker, start_date, end_date):
-    return data.DataReader(ticker, DATA_SOURCE, start_date, end_datec)
+def getStockData(ticker, start_date, end_date):
+    return data.DataReader(ticker, DATA_SOURCE, start_date, end_date)
